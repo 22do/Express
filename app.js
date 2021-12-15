65e7627b2020ca5072abc323d949b4c95b72dcd7
Doni <donisgemilang@gmail.com>
2021-12-15 20:22:57 +0700

    +app.js

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 let arr = [
-  {id : 33, name : 'Jhon'},
-  {id : 90 ,name : 'Cena'},
-  {id : 115 ,name : 'The Rock'}
-]
+  { id: 33, name: "Jhon" },
+  { id: 90, name: "Cena" },
+  { id: 115, name: "The Rock" },
+];
 
-let hasil 
-let tulisan = []
+let hasil;
+let tulisan = [];
 let ids = (x) => {
-  for(let i = 0; i<arr.length; i++){
-    if(arr[i].id == x){
-      tulisan = arr[i]
-      hasil = arr[i].id
-      return true
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id == x) {
+      tulisan = arr[i];
+      hasil = arr[i].id;
+      return true;
     }
   }
-}
+};
 
-app.get('/',(req,res)=>{
-  res.sendFile('./index.html',{root: __dirname})
-})
+app.get("/", (req, res) => {
+  res.sendFile("./index.html", { root: __dirname });
+});
 
-app.get('/students',(req,res)=>{
-  res.json(arr)
-})
+app.get("/students", (req, res) => {
+  res.json(arr);
+});
 
-app.get('/student/:idNyaStudent',(req,res)=>{
-  console.log(req.params.idNyaStudent)
-  let id = req.params.idNyaStudent
-  let ketemu = ids(id)
-  if(ketemu){
-    res.json(tulisan)
+app.get("/student/:idNyaStudent", (req, res) => {
+  console.log(req.params.idNyaStudent);
+  let id = req.params.idNyaStudent;
+  let ketemu = ids(id);
+  if (ketemu) {
+    res.json(tulisan);
+  } else {
+    res.status(404);
+    res.send("Page Not Found");
   }
-  else{
-    res.status(404)
-    res.send('Page Not Found')
-  }
-})
+});
 
 app.listen(port, () => {
-console.log('Berhasil')
-})
+  console.log("Berhasil");
+});
